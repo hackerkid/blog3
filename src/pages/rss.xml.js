0077ff96fe10bcbb65a645be16f900956691ec9b
Vishnu KS <yo@vishnuks.com>
2026-04-09 23:50:32 +0100

    fix the rss order.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,20 @@
 import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../config';
 
+const postImportResult = import.meta.glob('./blog/**/*.{md,mdx}', { eager: true });
+const posts = Object.values(postImportResult);
+
 export const get = () =>
 	rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: import.meta.env.SITE,
-		items: import.meta.glob('./blog/**/*.{md,mdx}'),
+		items: posts
+			.sort((a, b) => new Date(b.frontmatter.pubDate) - new Date(a.frontmatter.pubDate))
+			.map((post) => ({
+				link: post.url,
+				title: post.frontmatter.title,
+				pubDate: post.frontmatter.pubDate,
+				description: post.frontmatter.description,
+			})),
 	});
